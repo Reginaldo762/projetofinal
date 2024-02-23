109ea912246d042af0cc4ba0296de01ac49deb4c
[Reginaldo Grellmann] <[reginaldogrellmann@gmail.com]>
2024-02-22 21:00:03 -0300

    Atualização 22/2

---
+++ ProjFin---Front-End-main/conexao.php
@@ -0,0 +1,23 @@
+<?php
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "cuidadoso";
+
+    $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    //if($conn->connect_error){
+        //die("Conexão falhou!" . $conn->connect_error);
+    //}
+    //else{
+        //echo "Conexão efetuada com sucesso!";
+    //}
+
+
+
+
+
+
+
+
+?>
